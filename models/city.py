4e652acce836adf1461b7d3a6a40e3ec3e109be6
bdot100 <bidemiokunade@gmail.com>
2023-04-28 13:19:48 +0100

    Classes Added

--- models/city.py
+++ models/city.py
@@ -2,7 +2,8 @@
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import Column, Integer, String, ForeignKey, relationship
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
