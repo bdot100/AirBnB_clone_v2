99b8628b3ddd1bc5c365490029ac2ac9f317b552
bdot100 <bidemiokunade@gmail.com>
2023-04-28 14:10:51 +0100

    Console file edited

--- models/base_model.py
+++ models/base_model.py
@@ -21,10 +21,10 @@ class BaseModel:
         Args:
             *args (any): Unused.
             **kwargs (dict): Key/value pairs of attributes. """        
+        self.id = str(uuid.uuid4())
+        self.created_at = self.updated_at = datetime.utcnow()
+
         if kwargs:
-            self.id = str(uuid.uuid4())
-            self.created_at = self.updated_at = datetime.utcnow()
-            
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
@@ -33,13 +33,13 @@ class BaseModel:
 
     def __str__(self):
         """Returns a string representation of the instance"""
-        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
-        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
+        st = self.__dict__.copy()
+        st.pop("_sa_instance_state", None)
+        return "[{}] ({}) {}".format(type(self).__name__, self.id, st)
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
-        from models import storage
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.utcnow()
         models.storage.new(self)
         models.storage.save()
 
@@ -55,4 +55,4 @@ class BaseModel:
 
     def delete(self):
         """ Delete the current instance from the storage """
-        models.storage.delete(self);
+        models.storage.delete(self)
--- test.py
+++ test.py
@@ -1,1 +1,6 @@
 #!/usr/bin/python3
+
+import uuid
+
+
+print(str(uuid.uuid4()))
