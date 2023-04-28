fe1af8f5ae3ecac30eb37d8ac0880ef64499733a
bdot100 <bidemiokunade@gmail.com>
2023-04-28 10:16:18 +0100

    Console file improved and do_create imroved

--- console.py
+++ console.py
@@ -118,13 +118,30 @@ class HBNBCommand(cmd.Cmd):
         if not args:
             print("** class name missing **")
             return
-        elif args not in HBNBCommand.classes:
+        arg_list = args.split()
+        class_name = arg_list[0]
+        if class_name not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
-        new_instance = HBNBCommand.classes[args]()
-        storage.save()
+        
+        new_instance = HBNBCommand.classes[class_name]()
+        
+        for arg in arg_list[1:]:
+            param = arg.split('=')
+            key = param[0]
+            value = param[1]
+            
+            if value[0] == '\"':
+                value = value.replace('\"', '').replace('_', ' ')
+            elif '.' in value:
+                value = float(value)
+            else:
+                value = int(value)
+                
+            setattr(new_instance, key, value)
+        
+        new_instance.save()
         print(new_instance.id)
-        storage.save()
 
     def help_create(self):
         """ Help information for the create method """
