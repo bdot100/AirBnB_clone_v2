d45375aa2005159e54a2c5e7e485be215f77ed33
“Okunade <bidemiokunade@gmail.com>
2023-05-15 04:29:40 -0700

    Fabric Script Generated

---
+++ 1-pack_web_static.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""generates a .tgz archive from the contents 
+    of the web_static folder"""
+from fabric.api import local
+from datetime import datetime
+
+
+def do_pack():
+    """Generates a tgz archive from the contents of the web_static folder."""
+
+    # Create the folder if it doesn't exist
+    local('mkdir -p versions')
+
+    # Generate the name of the archive
+    time_format = "%Y%m%d%H%M%S"
+    archive_name = "web_static_" + datetime.now().strftime(time_format) + ".tgz"
+
+    # Create the path to the archive
+    archive_path = "versions/" + archive_name
+
+    # Create the archive
+    local("tar -czvf {} web_static".format(archive_path))
+
+    # Check if the archive was created
+    if local("test -f {}".format(archive_path)).failed:
+        return None
+
+    return archive_path
