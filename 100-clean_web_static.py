d340f7e01455ec0ef8444aa2d8a437d9c53a7622
bdot100 <bidemiokunade@gmail.com>
2023-05-16 16:21:09 +0100

    delete outdated archives

---
+++ 100-clean_web_static.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+# This Fab script deletes out-of-date archives.
+import os
+from fabric.api import *
+
+# Declear env.hosts
+env.hosts = ['ubuntu@35.174.204.79', 'ubuntu@34.227.91.64']
+
+
+def do_clean(number=0):
+    """This is used to delete out-of-date archives
+    Args:
+        number (int): The number of archives to keep.
+    
+    Instruction: If number is equal 0 or 1, keeps only
+    the most recent archive. If number is 2, keep the
+    most and second-most recent archives etc.
+    """
+    number = 1 if int(number) == 0 else int(number)
+
+    archives = sorted(os.listdir("versions"))
+    [archives.pop() for i in range(number)]
+    with lcd("versions"):
+        [local("rm ./{}".format(a)) for a in archives]
+
+    with cd("/data/web_static/releases"):
+        archives = run("ls -tr").split()
+        archives = [a for a in archives if "web_static_" in a]
+        [archives.pop() for i in range(number)]
+        [run("rm -rf ./{}".format(a)) for a in archives]
