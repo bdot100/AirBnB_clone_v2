b244b5d0d20ce97c2dade091398b47eea152c6ec
bdot100 <bidemiokunade@gmail.com>
2023-05-23 19:56:28 +0100

    Minimal App Created

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -2,8 +2,12 @@
 """ This script starts a Flask web application:
     web application is listening on 0.0.0.0, port 5000
     Routes: /: display “Hello HBNB!”
-    Routes: /hbnb displays HBNB
-    using the option strict_slashes=False in our route definition """
+    Routes: /hbnb displays "HBNB"
+    Routes: /c/<text>, display “C ” followed by the value
+    of the text variable (replace underscore _ symbols with
+    a space )
+    using the option strict_slashes=False in our route definition 
+"""
 from flask import Flask
 app = Flask(__name__)
 
@@ -18,6 +22,16 @@ def print_hello():
 def print_hbnb():
     """This method prints HBNB"""
     return ("HBNB")
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def print_ctext():
+    """display “C ” followed by the value
+    of the text variable (replace 
+    underscore _ symbols with a space )
+    """
+    text = text.replace('_', ' ')
+    return ('C' + ' ' + text)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
